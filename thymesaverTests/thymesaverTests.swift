e7123969c6f3cac4da81f40e33fc25072a05abed
Aiden Van Dyke <aidenavandyke@gmail.com>
2025-06-11 14:17:53 -0600

    Initial Commit

---
+++ thymesaverTests/thymesaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  thymesaverTests.swift
+//  thymesaverTests
+//
+//  Created by Aiden Van Dyke on 6/11/25.
+//
+
+import Testing
+@testable import thymesaver
+
+struct thymesaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
