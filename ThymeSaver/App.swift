c94b159213e2487d432aacc69207cb6ef0bde94e
Aiden Van Dyke <aidenavandyke@gmail.com>
2025-06-17 21:38:42 -0600

    Moved things around again

--- ThymeSaver/App.swift
+++ ThymeSaver/App.swift
@@ -14,7 +14,11 @@ struct thymesaverApp: App {
 struct AppContentView: View {
     @Environment(\.appDatabase) var appDatabase
     
-    @State private var selectedTab: Int = 2
+    init(_ defaultTab: Int = 0) {
+        _selectedTab.wrappedValue = defaultTab
+    }
+    
+    @State private var selectedTab: Int = 0
     
     var body: some View {
         TabView(selection: $selectedTab) {
